7ae782fa4697aa84e31042a4dfa7d2475303f6e6
Rushikesh Khedkar <118730223+DominiK037@users.noreply.github.com>
2025-10-21 02:21:52 +0530

    array(update-array-md): update Array.md and refactor files in package array

---
+++ linkedlist/ListNode.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class ListNode {
+    int value;
+    public ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+}
